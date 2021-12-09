79af05dcba791d1fe9306604252b64768719cd17
sato_tats <satotats.alive@gmail.com>
2021-12-10 01:12:56 +0900

    enable to get user info

--- src/main/kotlin/com/satotats/trial/auth0/Auth0.kt
+++ src/main/kotlin/com/satotats/trial/auth0/Auth0.kt
@@ -7,18 +7,21 @@ import io.ktor.auth.jwt.*
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.features.json.*
-import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.sessions.*
+import org.slf4j.LoggerFactory
 import java.util.concurrent.TimeUnit
 
+val log = LoggerFactory.getLogger("Auth0")
 
 @Suppress("unused")
 fun Application.main() {
     val httpClient = HttpClient(CIO) {
         install(JsonFeature) {
-            serializer = KotlinxSerializer()
+            serializer = JacksonSerializer()
         }
     }
 
@@ -28,6 +31,7 @@ fun Application.main() {
         .build()
 
     install(Authentication) {
+        // 使ってない
         jwt("jwt-auth0") {
             verifier(jwkProvider, System.getenv("AUTH0_CLIENT_DOMAIN"))
             validate { credential -> validateCreds(credential) }
@@ -35,41 +39,69 @@ fun Application.main() {
 
 
         oauth("oauth-auth0") {
-            urlProvider = { "http://localhost:8080/hello" }
+            urlProvider = { "http://localhost:8080/callback" }
             providerLookup = {
                 OAuthServerSettings.OAuth2ServerSettings(
                     name = "oauth",
-                    authorizeUrl = " https://${System.getenv("AUTH0_CLIENT_DOMAIN")}/",
-                    accessTokenUrl = "https://${System.getenv("AUTH0_CLIENT_DOMAIN")}/token",
+                    authorizeUrl = "https://${System.getenv("AUTH0_CLIENT_DOMAIN")}/authorize",
+                    accessTokenUrl = "https://${System.getenv("AUTH0_CLIENT_DOMAIN")}/oauth/token",
                     requestMethod = HttpMethod.Post,
                     clientId = System.getenv("AUTH0_CLIENT_ID"),
                     clientSecret = System.getenv("AUTH0_CLIENT_SECRET"),
-                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile")
+//                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile")
                 )
             }
             client = httpClient
         }
     }
 
+    install(Sessions) {
+        cookie<UserSession>("user_session")
+    }
+
     routing {
-        get("/login") {
-            // Redirects to 'authorizeUrl' automatically
+        authenticate("oauth-auth0") {
+            get("/login") {
+                // Redirects to 'authorizeUrl' automatically
+            }
+
+            get("/callback") {
+                val principal: OAuthAccessTokenResponse.OAuth2? = call.principal()
+                log.info(principal?.accessToken)
+//                call.response.headers.append(HttpHeaders.Authorization, "Bearer ${principal?.accessToken.toString()}")
+                call.sessions.set(UserSession(principal?.accessToken.toString()))
+                call.respondRedirect("/hello")
+            }
+
         }
 
-        authenticate("oauth-auth0") {
-            get("/hello") {
-                val principal = call.principal<JWTPrincipal>()
-
-                if (principal == null) {
-                    call.respond(":-(")
-                    return@get
+        get("hello") {
+            val userSession: UserSession? = call.sessions.get<UserSession>()
+            if (userSession != null) {
+                val userInfo: UserInfo = httpClient.get("https://${System.getenv("AUTH0_CLIENT_DOMAIN")}/userinfo") {
+                    headers {
+                        append(HttpHeaders.Authorization, "Bearer ${userSession.token}")
+                    }
                 }
-                principal.payload.getClaim("name").let { name ->
-                    call.respond("You've successfully logged-in. Hello, $name")
-                }
-
+                call.respond(userInfo.toString())
+            } else {
+                call.respondRedirect("/")
             }
         }
+
+//        authenticate("jwt-auth0") {
+//            get("/hello") {
+//                val principal = call.principal<JWTPrincipal>()
+//                log.info(principal.toString())
+//                if (principal == null) {
+//                    call.respond(":-(")
+//                    return@get
+//                }
+//                principal.payload.getClaim("name").let { name ->
+//                    call.respond("You've successfully logged-in. Hello, $name")
+//                }
+//            }
+//        }
     }
 }
 
@@ -82,3 +114,7 @@ fun validateCreds(credential: JWTCredential): JWTPrincipal? {
 
     return null
 }
+
+data class UserSession(val token: String)
+
+data class UserInfo(val name: String?)
